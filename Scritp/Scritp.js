ad92ccfde4099906afe8eca9977579a81a643a6c
antoniolopgam <108624637+antoniolopgam@users.noreply.github.com>
2022-07-05 11:03:14 +0200

    home

---
+++ Scritp/Scritp.js
@@ -0,0 +1,12 @@
+$(function(){
+    $("#menuHamburguesa").click(function(){
+        $("#opcionesMenu").stop()
+        $("#opcionesMenu").animate({
+            height: "toggle"
+        },500)
+        $("span:nth-child(1)").toggleClass("primera")
+        $("span:nth-child(2)").toggleClass("segunda")
+        $("span:nth-child(3)").toggleClass("tercera")
+    })
+ })
+ 
